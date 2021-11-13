43d6a59e4891dfb25f223dae173f7a70eef068b2
mkolodzheieva <mariana.kolodzheieva@intellias.com>
2021-11-13 12:48:24 +0200

    add CRUD operations

--- src/main/java/com/hillel/users/controller/UserController.java
+++ src/main/java/com/hillel/users/controller/UserController.java
@@ -1,14 +1,86 @@
 package com.hillel.users.controller;
 
+import com.hillel.users.domain.User;
+import com.hillel.users.domain.UserRepository;
+import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+  UserRepository userRepository;
+
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping("/users/add")
+  public String userForm(User user) {
+    return "add-user";
+  }
+
+  @PostMapping("/users/add")
+  public String addUser(@Valid User user, BindingResult result) {
+
+    if (result.hasErrors()) {
+      return "add-user";
+    }
+
+    userRepository.save(user);
+    return "redirect:/users";
+
+  }
+
   @GetMapping("/users")
-  public String getUsers() {
+  public String getUsers(Model model) {
+    model.addAttribute("users", userRepository.findAll());
     return "users";
+  }
+
+  @GetMapping("/users/delete/{id}")
+  public String deleteUser(@PathVariable("id") long id) {
+
+    Optional<User> user = userRepository.findById(id);
+
+    if (user.isPresent()) {
+      userRepository.delete(user.get());
+    }
+
+    return "redirect:/users";
+  }
+
+  @GetMapping("/users/edit/{id}")
+  public String editUser(@PathVariable("id") long id, Model model) {
+
+    Optional<User> user = userRepository.findById(id);
+
+    if (user.isEmpty()) {
+      throw new IllegalArgumentException("Invalid user id: " + id);
+    }
+
+    model.addAttribute("user", user.get());
+    return "update-user";
+
+  }
+
+  @PostMapping("/users/update/{id}")
+  public String updateUser(@PathVariable("id") long id, @Valid User user,
+      BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors()) {
+      user.setId(id);
+      return "update-user";
+    }
+
+    userRepository.save(user);
+    return "redirect:/users";
+
   }
 
 }
